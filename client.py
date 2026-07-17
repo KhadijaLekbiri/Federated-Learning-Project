c64a70c94c16ae1ee77ae77aef7cd29d31fe283a
KhadijaLekbiri <lekbirikhadija3@gmail.com>
2026-07-17 12:18:25 +0100

    Add Fed Avg method

--- client.py
+++ client.py
@@ -3,20 +3,26 @@ import pandas as pd
 import numpy as np
 import copy
 
+import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from model import NetflowClassifier
 
 
 class Client:
-    def __init__(self, client_id, dataset, input_dim, batch_size=32, device="cpu"):
+    def __init__(self, client_id, dataset, input_dim, local_lr=0.01,local_epochs=1, batch_size=32, device="cpu"):
         self.client_id = client_id
         self.dataset =  dataset
         self.batch_size = batch_size
         self.input_dim = input_dim
         self.dataloader =  DataLoader(dataset, shuffle=True, batch_size=batch_size)
         self.device = device
+
+        self.local_epochs = local_epochs
+        self.local_lr = local_lr
+
         self.last_gradient = None
+        self.last_weights = None
         self.model = None
 
     
@@ -24,12 +30,10 @@ class Client:
         self.model = NetflowClassifier(self.input_dim).to(self.device)
         self.model.load_state_dict(copy.deepcopy(global_state_dict))
         
-
-    def local_train(self):
-
-        if self.model is None:
-            raise RuntimeError("Client has no model yet")
         
+        
+    def _train_fedsgd(self):
+
         self.model.train()
         loss_fn = nn.CrossEntropyLoss()
 
@@ -49,13 +53,27 @@ class Client:
         self.last_gradient = gradients
         return gradients, loss.item()
     
-    def get_update(self):
-        return self.last_gradient
+    def _train_fedavg(self):
+
+        self.model.train()
+        loss_fn = nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=self.local_lr)
+        
+        total_loss, n_batches = 0.0, 0
+        for _ in range(self.local_epochs):
+            for x, y in self.dataloader:
+                x, y = x.to(self.device), y.to(self.device)
+                optimizer.zero_grad()
+                output = self.model(x)
+                loss = loss_fn(output, y)
+                loss.backward()
+                optimizer.step()
+                total_loss += loss.item()
+                n_batches += 1
+        avg_loss = total_loss / max(n_batches, 1)
+
+        weights = copy.deepcopy(self.model.state_dict())
+        
+        self.last_weights = weights
+        return weights, avg_loss
     
-
-
-
-# df = pd.read_parquet("NF-UNSW-NB15-V2.parquet")
-# print(df.head)
-# client = Client(client_id=0, dataset=df)
-# client.receive_model(model.statedict())
