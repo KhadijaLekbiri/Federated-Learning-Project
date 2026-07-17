0ed901f35fac24f15399e3291268c303f968ed26
KhadijaLekbiri <lekbirikhadija3@gmail.com>
2026-07-17 12:20:11 +0100

    Use both FedAVG and FedSGD

--- server.py
+++ server.py
@@ -3,6 +3,8 @@
 import copy
 import torch
 
+from evaluate import evaluate
+
 class Server:
     def __init__(self, initial_model,lr=0.01,use_adam=False):
         self.model = initial_model
@@ -12,14 +14,14 @@ class Server:
                     torch.optim.SGD(self.model.parameters(), lr=lr)
                 )
     
-    def FL_round(self, clients):
+    def FedSGD_round(self, clients):
         
         averaged_grads = {name: torch.zeros_like(param) for name, param in self.model.named_parameters()}
         
         for client in clients:
             client.receive_model(self.model.state_dict())
 
-            gradients, loss = client.local_train()
+            gradients, loss = client._train_fedsgd()
 
             print("\tLoss:", loss)
             for name, grad in gradients.items():
@@ -33,12 +35,38 @@ class Server:
             param.grad = averaged_grads[name]
         self.optimizer.step()
 
-    def FL_algo(self, clients, num_rounds):
-        c = num_rounds
-        while c:
-            self.FL_round(clients)
-            print("************************")
-            c -= 1
             
-
+    def FedAVG_round(self, clients):
         
+        averaged_weights = {name: torch.zeros_like(param) for name, param in self.model.named_parameters()}
+        
+        for client in clients:
+            client.receive_model(self.model.state_dict())
+
+            weights, loss = client._train_fedavg()
+
+            print("\tLoss:", loss)
+            for name, grad in weights.items():
+                averaged_weights[name] += grad
+
+        for name in averaged_weights:
+            averaged_weights[name] /= len(clients)
+
+        self.model.load_state_dict(averaged_weights)
+        
+
+    def Fed_train(self, clients, num_rounds, X_test, y_test, mode="fedsgd"):
+        round_num = num_rounds
+        while round_num:
+            if mode == "fedsgd":
+                self.FedSGD_round(clients)
+            elif mode == "fedavg":
+                self.FedAVG_round(clients)
+            else:
+                raise ValueError(f"Unknown mode: {mode}")
+
+            metrics = evaluate(self.model, X_test, y_test)
+            print(f"Round {round_num}: {metrics}")
+            print("************************")
+            round_num -= 1
+    
